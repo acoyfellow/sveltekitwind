a1bc83e4fd8be872c73800eccc4bb2e9e517140f
Jordan Coeyman <coeyman@gmail.com>
2021-01-30 18:54:38 +0000

    first commit

---
+++ snowpack.config.js
@@ -0,0 +1,20 @@
+// Consult https://www.snowpack.dev to learn about these options
+module.exports = {
+  extends: '@sveltejs/snowpack-config',
+  mount: {
+    'src/components': '/_components'
+  },
+  alias: {
+    $components: './src/components'
+  },
+  plugins: [
+    [
+      '@snowpack/plugin-build-script',
+      {
+        cmd: "postcss",
+        input: [".css", ".pcss"],
+        output: [".css"],
+      }
+    ]
+  ]
+};
